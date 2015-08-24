0d78748c10a5718ed455ec969b2b3025c2e60fcd
Cuiyn <Cuiyn@users.noreply.github.com>
2015-08-24 08:45:57 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+from app import app
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SECRET_KEY']='you-will-never-guess-hhh'
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
